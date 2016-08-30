7db6fd07d5f64d2c4cb375c32d370874ba18f62e
Rachel Seale <rvseale@sky.com>
2016-08-30 20:51:12 +0100

    creating scss files and simple angular form

---
+++ javascripts/form.js
@@ -0,0 +1,15 @@
+(function(angular) {
+  'use strict';
+angular.module('formExample', [])
+  .controller('ExampleController', ['$scope', function($scope) {
+
+    $scope.dob = {
+      value: new Date()
+    };
+    $scope.master = {};
+
+    $scope.update = function(user) {
+      console.log($scope.user);
+    };
+  }]);
+})(window.angular);
