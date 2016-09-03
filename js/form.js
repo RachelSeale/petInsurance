8a9fe550d9e1e7626e9ce46d08c53f238e51c6bf
Rachel Seale <rvseale@sky.com>
2016-09-03 17:23:27 +0100

    fixing some validation issues and styles

--- js/form.js
+++ js/form.js
@@ -4,11 +4,6 @@
   angular.module('formExample', [])
     .controller('ExampleController', ['$scope', function($scope) {
 
-      $scope.dob = {
-        value: new Date()
-      };
-      $scope.master = {};
-
       $scope.update = function(user) {
         console.log($scope.user);
       };
